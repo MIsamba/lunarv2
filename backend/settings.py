00df00c7127dbc3a9a5eb2e57944335555692e0c
MIsamba <isambagm@gmail.com>
2021-11-18 14:27:02 +0530

    statifiles setup

--- backend/settings.py
+++ backend/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
@@ -130,9 +133,6 @@ STATICFILES_DIRS = [
     
 ]
 
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
